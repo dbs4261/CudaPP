d7d8068714f78eac12f05ecca6db1d62bc2301ed
Daniel Simon <Dan.Simon@ogsystems.com>
2020-02-18 17:05:50 -0800

    Added proper exceptions to cuda_array.h

--- arrays/cuda_array.h
+++ arrays/cuda_array.h
@@ -12,6 +12,7 @@
 
 #include <channel_descriptor.h>
 
+#include "exceptions/cuda_exception.h"
 #include "utilities/memory_helpers.h"
 #include "vector_types/vector_type_traits.h"
 
@@ -21,40 +22,50 @@ template <typename T>
 class CudaArray {
   static_assert(not std::is_same<T, void>::value, "Template type cannot be void");
  public:
-  CudaArray() : CudaArray<T>(1, 1, 1) {}
-  CudaArray(std::size_t w, std::size_t h=0, std::size_t d=0, unsigned int _flags=0)
+  CudaArray() noexcept(false) : CudaArray<T>(1, 1, 1) {}
+  CudaArray(std::size_t w, std::size_t h=0, std::size_t d=0, unsigned int _flags=0) noexcept(false)
       : CudaArray(make_cudaExtent(w, h, d), _flags) {}
-  explicit CudaArray(cudaExtent _extent, unsigned int _flags=0)
+  explicit CudaArray(cudaExtent _extent, unsigned int _flags=0) noexcept(false)
       : extent(_extent), array(nullptr), flags(_flags) {
     auto desc = cudaCreateChannelDesc<T>();
-    CudaCatchError(cudaMalloc3DArray(&array, &desc, extent, flags));
+    cudaError_t ret = cudaMalloc3DArray(&array, &desc, extent, flags);
+    if (ret != cudaSuccess) {
+      throw CudaException(ret);
+    }
   }
-  CudaArray(const T* _data, std::size_t w, std::size_t h=0, std::size_t d=0, unsigned int _flags=0)
+  CudaArray(const T* _data, std::size_t w, std::size_t h=0, std::size_t d=0, unsigned int _flags=0) noexcept(false)
       : CudaArray<T>(_data, make_cudaExtent(w, h, d), _flags) {}
-  CudaArray(const T* _data, cudaExtent _extent, unsigned int _flags=0)
+  CudaArray(const T* _data, cudaExtent _extent, unsigned int _flags=0) noexcept(false)
       : CudaArray<T>(_extent, _flags) {
     this->Set(_data);
   }
 
-  CudaArray(const CudaArray<T>& other) : CudaArray(other, other.flags) {}
-  CudaArray(const CudaArray<T>& other, unsigned int _flags=0) : CudaArray(other.extent, _flags) {
+  CudaArray(const CudaArray<T>& other) noexcept(false) : CudaArray(other, other.flags) {}
+  CudaArray(const CudaArray<T>& other, unsigned int _flags=0) noexcept(false) : CudaArray(other.extent, _flags) {
     this->Set(other);
   }
   CudaArray(CudaArray<T>&& other) noexcept : extent(std::move(extent)), array(std::move(other.array)), flags(other.flags) {
     other.array = nullptr;
   }
 
-  ~CudaArray() {
-    CudaCatchError(cudaFreeArray(this->array));
+  ~CudaArray() noexcept {
+    cudaError_t ret = cudaFreeArray(this->array);
+    if (ret != cudaSuccess) {
+      // Uncatchable warning
+      throw CudaException(ret);
+    }
   }
 
-  CudaArray<T>& operator=(const CudaArray<T>& other) {
+  CudaArray<T>& operator=(const CudaArray<T>& other) noexcept(false) {
     if (other.array != this->array) {
-      CudaCatchError(cudaFreeArray(this->array));
+      cudaError_t ret = cudaFreeArray(this->array);
+      if (ret != cudaSuccess) {
+        throw CudaException(ret);
+      }
+      this->extent = other.extent;
+      this->flags = other.flags;
+      this->array = other.array;
     }
-    this->extent = other.extent;
-    this->flags = other.flags;
-    this->array = other.array;
     return *this;
   }
 
@@ -66,49 +77,63 @@ class CudaArray {
     return *this;
   }
 
-  const cudaExtent& Extent() const {
+  const cudaExtent& Extent() const noexcept {
     return this->extent;
   }
 
-  cudaArray* ArrayPtr() {
+  cudaArray* ArrayPtr() noexcept{
     return this->array;
   }
-  const cudaArray* ArrayPtr() const {
+  const cudaArray* ArrayPtr() const noexcept {
     return this->array;
   }
 
-  void Set(const T* _data) {
+  void Set(const T* _data) noexcept(false) {
     cudaMemcpy3DParms params = Memcpy3DParamsHD<T>(_data, this->array, this->extent);
-    CudaCatchError(cudaMemcpy3D(&params));
+    cudaError_t ret = cudaMemcpy3D(&params);
+    if (ret != cudaSuccess) {
+      throw CudaException(ret);
+    }
   }
 
-  void Set(const CudaArray<T>& other) {
-    assert(this->Size() == other.Size());
-    cudaMemcpy3DParms params = Memcpy3DParamsDD(other.array, this->array, this->extent);
-    CudaCatchError(cudaMemcpy3D(&params));
+  void Set(const CudaArray<T>& other) noexcept(false) {
+    if (this->array != other.array) {
+      assert(this->Size() == other.Size());
+      cudaMemcpy3DParms params = Memcpy3DParamsDD(other.array, this->array, this->extent);
+      cudaError_t ret = cudaMemcpy3D(&params);
+      if (ret != cudaSuccess) {
+        throw CudaException(ret);
+      }
+    }
   }
 
-  void Get(T* _data) const {
+  void Get(T* _data) const noexcept(false) {
     cudaMemcpy3DParms params = Memcpy3DParamsDH<T>(this->array, _data, this->extent);
-    CudaCatchError(cudaDeviceSynchronize());
-    CudaCatchError(cudaMemcpy3D(&params));
+    cudaError_t ret = cudaDeviceSynchronize();
+    if (ret != cudaSuccess) {
+      throw CudaException(ret);
+    }
+    ret = cudaMemcpy3D(&params);
+    if (ret != cudaSuccess) {
+      throw CudaException(ret);
+    }
   }
 
-  std::size_t Size() const {
+  std::size_t Size() const noexcept {
     return (this->extent.width != 0 ? this->extent.width : 1) *
            (this->extent.height != 0 ? this->extent.height : 1) *
            (this->extent.depth != 0 ? this->extent.depth : 1);
   }
 
-  std::size_t Width() const {
+  std::size_t Width() const noexcept {
     return this->extent.width;
   }
 
-  std::size_t Height() const {
+  std::size_t Height() const noexcept {
     return this->extent.height;
   }
 
-  std::size_t Depth() const {
+  std::size_t Depth() const noexcept {
     return this->extent.depth;
   }
 
