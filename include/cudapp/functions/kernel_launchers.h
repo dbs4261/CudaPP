07de1b5bd59eee8e9e009e815403e50703c428cc
Daniel Simon <Dan.Simon@ogsystems.com>
2020-03-29 15:57:16 -0700

    Added comments!

--- include/cudapp/functions/kernel_launchers.h
+++ include/cudapp/functions/kernel_launchers.h
@@ -27,6 +27,9 @@ enum struct CooperativeMultiDeviceFlags : unsigned int {
              cudaCooperativeLaunchMultiDeviceNoPostSync,
 };
 
+/**
+ * @brief A helper function to combine flags.
+ */
 [[nodiscard]] constexpr inline CooperativeMultiDeviceFlags operator|(CooperativeMultiDeviceFlags a, CooperativeMultiDeviceFlags b) {
   using U = std::underlying_type_t<CooperativeMultiDeviceFlags>;
   return static_cast<CooperativeMultiDeviceFlags>(static_cast<U>(a) | static_cast<U>(b));
@@ -34,9 +37,23 @@ enum struct CooperativeMultiDeviceFlags : unsigned int {
 
 namespace detail {
 
+/**
+ * @brief A helper to prevent the function arguments being used for template argument deduction instead of the function.
+ * @tparam T The type.
+ */
 template<typename T> struct identity {using type = T;};
 template<typename T> using identity_t = typename identity<T>::type;
 
+/**
+ * @brief Does the actual work of launching a kernel.
+ * @tparam Args The type of the arguments to be passed to the kernel.
+ * @param grid The 3d grid size that the kernel will be launched with.
+ * @param block The 3d block size that the kernel will be launched with.
+ * @param shared_memory The amount of shared memory in bytes allocated per block.
+ * @param stream The stream to launch the kernel on.
+ * @param function The kernel to watch.
+ * @param args The arguments to pack to the kernel.
+ */
 template <typename ... Args>
 void LaunchKernelHelper(dim3 grid, dim3 block, std::size_t shared_memory, cudaStream_t stream, void(*function)(Args...), identity_t<Args> ... args) {
   std::array<void*, sizeof...(Args)> args_array = {std::addressof(args)...};
@@ -50,6 +67,16 @@ void LaunchKernelHelper(dim3 grid, dim3 block, std::size_t shared_memory, cudaSt
   }
 }
 
+/**
+ * @brief Does the actual work of launching the cooperative kernel.
+ * @tparam Args The type of the arguments to be passed to the kernel.
+ * @param grid The 3d grid size that the kernel will be launched with.
+ * @param block The 3d block size that the kernel will be launched with.
+ * @param shared_memory The amount of shared memory in bytes allocated per block.
+ * @param stream The stream to launch the kernel on.
+ * @param function The cooperative kernel to watch.
+ * @param args The arguments to pack to the kernel.
+ */
 template <typename ... Args>
 void LaunchCooperativeKernelHelper(dim3 grid, dim3 block, std::size_t shared_memory, cudaStream_t stream, void(*function)(Args...), identity_t<Args> ... args) {
   std::array<void*, sizeof...(Args)> args_array = {std::addressof(args)...};
@@ -63,20 +90,65 @@ void LaunchCooperativeKernelHelper(dim3 grid, dim3 block, std::size_t shared_mem
   }
 }
 
+/**
+ * @brief A pair consisting of a function and an array of pointers to the data that it is called with.
+ */
+template <typename ... Args>
+using HostFunctionnData = std::pair<void(*)(Args...), std::array<void*, sizeof...(Args)>>;
+
+/**
+ * @brief A wrapper around a function to be launched from cudaLaunchHostFunc to allow more arguments to be passed.
+ * @tparam Args The type of the arguments to be passed to the packed function.
+ * @param user_data The wrapped function along with the calling parameters.
+ */
+template <typename ... Args>
+void HostFunctionWrapper(void* user_data) {
+  // TODO(Daniel.Simon): check if the passing of the pointer array causes issues with object lifetime. It may be necessary to forward as tuple.
+  auto* actual_data = reinterpret_cast<HostFunctionnData<Args...>*>(user_data);
+  actual_data->first(*reinterpret_cast<std::add_pointer_t<Args>>(actual_data->second)...);
+}
+
 }
 
 //<editor-fold desc="SimpleKernelLaunchers">
 
+/**
+ * @brief Launches a standard cuda kernel.
+ * @tparam Args The types of the arguments for the Kernel.
+ * @param grid The 3d grid size that the kernel will be launched with.
+ * @param block The 3d block size that the kernel will be launched with.
+ * @param shared_memory The amount of shared memory in bytes allocated per block.
+ * @param stream The stream to launch the kernel on.
+ * @param function The kernel to launch.
+ * @param args The arguments to pass to the kernel.
+ */
 template <typename ... Args>
 void LaunchKernel(dim3 grid, dim3 block, std::size_t shared_memory, cudaStream_t stream, void(*function)(Args...), detail::identity_t<Args>&& ... args) {
   detail::LaunchKernelHelper(grid, block, shared_memory, stream, function, std::forward<Args>(args)...);
 }
 
+/**
+ * @brief Launches a standard cuda kernel.
+ * @tparam Args The types of the arguments for the Kernel.
+ * @param grid The 3d grid size that the kernel will be launched with.
+ * @param block The 3d block size that the kernel will be launched with.
+ * @param shared_memory The amount of shared memory in bytes allocated per block.
+ * @param function The kernel to launch.
+ * @param args The arguments to pass to the kernel.
+ */
 template <typename ... Args>
 void LaunchKernel(dim3 grid, dim3 block, std::size_t shared_memory, void(*function)(Args...), detail::identity_t<Args>&& ... args) {
   detail::LaunchKernelHelper(grid, block, shared_memory, cudaStream_t{0}, function, std::forward<Args>(args)...);
 }
 
+/**
+ * @brief Launches a standard cuda kernel.
+ * @tparam Args The types of the arguments for the Kernel.
+ * @param grid The 3d grid size that the kernel will be launched with.
+ * @param block The 3d block size that the kernel will be launched with.
+ * @param function The kernel to launch.
+ * @param args The arguments to pass to the kernel.
+ */
 template <typename ... Args>
 void LaunchKernel(dim3 grid, dim3 block, void(*function)(Args...), detail::identity_t<Args>&& ... args) {
   detail::LaunchKernelHelper(grid, block, std::size_t{0}, cudaStream_t{0}, function, std::forward<Args>(args)...);
@@ -85,29 +157,72 @@ void LaunchKernel(dim3 grid, dim3 block, void(*function)(Args...), detail::ident
 
 //<editor-fold desc="CooperativeKernelLaunchers">
 
+/**
+ * @brief Launches a kernel that cooperates between blocks.
+ * @tparam Args The types of the arguments for the Kernel.
+ * @param grid The 3d grid size that the kernel will be launched with.
+ * @param block The 3d block size that the kernel will be launched with.
+ * @param shared_memory The amount of shared memory in bytes allocated per block.
+ * @param stream The stream to launch the kernel on.
+ * @param function The kernel to launch.
+ * @param args The arguments to pass to the kernel.
+ */
 template <typename ... Args>
 void LaunchCooperativeKernel(dim3 grid, dim3 block, std::size_t shared_memory, cudaStream_t stream, void(*function)(Args...), detail::identity_t<Args>&& ... args) {
   detail::LaunchCooperativeKernelHelper(grid, block, shared_memory, stream, function, std::forward<Args>(args)...);
 }
 
+/**
+ * @brief Launches a kernel that cooperates between blocks.
+ * @tparam Args The types of the arguments for the Kernel.
+ * @param grid The 3d grid size that the kernel will be launched with.
+ * @param block The 3d block size that the kernel will be launched with.
+ * @param shared_memory The amount of shared memory in bytes allocated per block.
+ * @param function The kernel to launch.
+ * @param args The arguments to pass to the kernel.
+ */
 template <typename ... Args>
 void LaunchCooperativeKernel(dim3 grid, dim3 block, std::size_t shared_memory, void(*function)(Args...), detail::identity_t<Args>&& ... args) {
   detail::LaunchCooperativeKernelHelper(grid, block, shared_memory, cudaStream_t{0}, function, std::forward<Args>(args)...);
 }
 
+/**
+ * @brief Launches a kernel that cooperates between blocks.
+ * @tparam Args The types of the arguments for the Kernel.
+ * @param grid The 3d grid size that the kernel will be launched with.
+ * @param block The 3d block size that the kernel will be launched with.
+ * @param function The kernel to launch.
+ * @param args The arguments to pass to the kernel.
+ */
 template <typename ... Args>
 void LaunchCooperativeKernel(dim3 grid, dim3 block, void(*function)(Args...), detail::identity_t<Args>&& ... args) {
   detail::LaunchCooperativeKernelHelper(grid, block, std::size_t{0}, cudaStream_t{0}, function, std::forward<Args>(args)...);
 }
 //</editor-fold>
 
+/**
+ * @brief A factory style method for launching a cooperative kernel across multiple devices.
+ * @tparam Args The types of the arguments of the kernel to be launched.
+ */
 template <typename ... Args>
 class MultiDeviceCoorperativeLauncher {
  public:
+  /**
+   * @brief Create the launcher with the parameters that must be consistent across streams.
+   * @param _grid The 3d grid size that the kerenels will be launched with.
+   * @param _block The 3d block size that the kernels will be launched with.
+   * @param _shared_memory The amount of shared memory in bytes that the kernels will be launched with.
+   * @param _function The kernel that will be launched in each stream.
+   * @param _flags The synchronization options.
+   */
   explicit MultiDeviceCoorperativeLauncher(dim3 _grid, dim3 _block, std::size_t _shared_memory,
       void(*_function)(Args...), CooperativeMultiDeviceFlags _flags=CooperativeMultiDeviceFlags::BothSync) :
       grid(_grid), block(_block), shared_memory(_shared_memory), function(_function), flags(_flags), arguments() {}
 
+  /**
+   * @brief Gets the number of streams with arguments set to be launched.
+   * @return The number of streams to be launched.
+   */
   [[nodiscard]] auto CooperativeStreams() const {
     return arguments.size();
   }
@@ -126,10 +241,20 @@ class MultiDeviceCoorperativeLauncher {
     return *this;
   }
 
+  /**
+   * @brief Get the pointer array to the arguments for the given stream.
+   * @param stream The stream the arguments are associated with.
+   * @return A modifiable array of pointers to the arguments.
+   */
   [[nodiscard]] std::array<void*, sizeof...(Args)>& StreamArgumentPointers(cudaStream_t stream) {
     return arguments.at(stream);
   }
 
+  /**
+   * @brief Get the pointer array to the arguments for the given stream.
+   * @param stream The stream the arguments are associated with.
+   * @return A constant array of pointers to the arguments.
+   */
   [[nodiscard]] const std::array<void*, sizeof...(Args)>& StreamArgumentPointers(cudaStream_t stream) const {
     return arguments.at(stream);
   }
@@ -164,6 +289,27 @@ class MultiDeviceCoorperativeLauncher {
   void(*function)(Args...);
   std::unordered_map<cudaStream_t, std::array<void*, sizeof...(Args)>> arguments;
 };
+
+/**
+ * @brief Adds a CPU function to be executed on a stream.
+ * @tparam Args The type of the arguments for the given function.
+ * @param stream The stream that the host function should be queued in.
+ * @param function A void returning function that runs on the CPU.
+ * @param args The arguments needed to launch the host function.
+ */
+template <typename ... Args>
+void LaunchHostFunction(cudaStream_t stream, void(*function)(Args...), detail::identity_t<Args> ... args) {
+  std::array<void*, sizeof...(Args)> args_array = {std::addressof(args)...};
+  detail::HostFunctionnData<Args...> user_data(function, args_array.data());
+  cudaError_t ret = cudaLaunchHostFunc(stream, detail::HostFunctionWrapper<Args...>, user_data);
+  if (ret != cudaSuccess) {
+    throw CudaException(ret);
+  }
+  ret = cudaGetLastError();
+  if (ret != cudaSuccess) {
+    throw CudaException(ret);
+  }
+}
 
 }
 
